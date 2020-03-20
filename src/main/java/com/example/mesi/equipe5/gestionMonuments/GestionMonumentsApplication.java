fb8fbc20c163943734b78c136e9e45e0d81023be
twill78 <56994544+twill78@users.noreply.github.com>
2020-03-20 10:46:30 +0100

    Add files via upload

---
+++ src/main/java/com/example/mesi/equipe5/gestionMonuments/GestionMonumentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.mesi.equipe5.gestionMonuments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionMonumentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionMonumentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mesi/equipe5/gestionMonuments/GestionMonumentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mesi.equipe5.gestionMonuments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionMonumentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
