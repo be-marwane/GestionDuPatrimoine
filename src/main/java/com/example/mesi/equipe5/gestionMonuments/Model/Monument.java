4c5cb04bfa1f6b2d4bc0ae6785da6c13f89bd1c7
unknown <marwane.berrima@gmail.com>
2020-05-11 15:34:56 +0200

    changement d'attribut pour la BDD

--- src/main/java/com/example/mesi/equipe5/gestionMonuments/Model/Monument.java
+++ src/main/java/com/example/mesi/equipe5/gestionMonuments/Model/Monument.java
@@ -12,45 +12,66 @@ public class Monument {
 	@GeneratedValue
 	private Long id;
 	
-	private Long latitude;
-	private Long longitude;
-	private String titre;
-	private String url;
-	private String  description;
-	private String iconUrl;
-	private String pays;
-	private String ville;
-	private String wikiUrl;
-	private String photoUrl;
+	private String appellationCourante;
+	private Integer siecle;
+	private String precisionSurLaProtection;
+	private String dateDeProtection;
+	private String  statut;
+	private String description;
+	private String historique;
+	private String auteur;
+	private String adresse;
+	private String siecleFormatBrut;
+	private String region;
+	private String departement;
+	private String commune;
+	private String affectataire;
+	private String niveauDeProtection;
+	private String adresse1;
+	private String Commune1;
+	private Integer codeDepartement;
+	private String ancienneRegion;
+	private String DMAJ;
+	private String reference;
+	private String base;
+	private Integer INSEE;
+	private String coordonneesBan;
+	
 	public Long getId() {
 		return id;
 	}
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public Long getLatitude() {
-		return latitude;
+	public String getAppellationCourante() {
+		return appellationCourante;
 	}
-	public void setLatitude(Long latitude) {
-		this.latitude = latitude;
+	public void setAppellationCourante(String appellationCourante) {
+		this.appellationCourante = appellationCourante;
 	}
-	public Long getLongitude() {
-		return longitude;
+	public Integer getSiecle() {
+		return siecle;
 	}
-	public void setLongitude(Long longitude) {
-		this.longitude = longitude;
+	public void setSiecle(Integer siecle) {
+		this.siecle = siecle;
 	}
-	public String getTitre() {
-		return titre;
+	public String getPrecisionSurLaProtection() {
+		return precisionSurLaProtection;
 	}
-	public void setTitre(String titre) {
-		this.titre = titre;
+	public void setPrecisionSurLaProtection(String précisionSurLaProtection) {
+		this.precisionSurLaProtection = précisionSurLaProtection;
 	}
-	public String getUrl() {
-		return url;
+	public String getDateDeProtection() {
+		return dateDeProtection;
 	}
-	public void setUrl(String url) {
-		this.url = url;
+	public void setDateDeProtection(String dateDeProtection) {
+		this.dateDeProtection = dateDeProtection;
+	}
+	public String getStatut() {
+		return statut;
+	}
+	public void setStatut(String statut) {
+		this.statut = statut;
 	}
 	public String getDescription() {
 		return description;
@@ -58,36 +79,115 @@ public class Monument {
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	public String getIconUrl() {
-		return iconUrl;
+	public String getHistorique() {
+		return historique;
 	}
-	public void setIconUrl(String iconUrl) {
-		this.iconUrl = iconUrl;
+	public void setHistorique(String historique) {
+		this.historique = historique;
 	}
-	public String getPays() {
-		return pays;
+	public String getAuteur() {
+		return auteur;
 	}
-	public void setPays(String pays) {
-		this.pays = pays;
+	public void setAuteur(String auteur) {
+		this.auteur = auteur;
 	}
-	public String getVille() {
-		return ville;
+	public String getAdresse() {
+		return adresse;
 	}
-	public void setVille(String ville) {
-		this.ville = ville;
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
 	}
-	public String getWikiUrl() {
-		return wikiUrl;
+	public String getSiecleFormatBrut() {
+		return siecleFormatBrut;
 	}
-	public void setWikiUrl(String wikiUrl) {
-		this.wikiUrl = wikiUrl;
+	public void setSiecleFormatBrut(String siecleFormatBrut) {
+		this.siecleFormatBrut = siecleFormatBrut;
 	}
-	public String getPhotoUrl() {
-		return photoUrl;
+	public String getRegion() {
+		return region;
 	}
-	public void setPhotoUrl(String photoUrl) {
-		this.photoUrl = photoUrl;
+	public void setRegion(String region) {
+		this.region = region;
 	}
+	public String getDepartement() {
+		return departement;
+	}
+	public void setDepartement(String departement) {
+		this.departement = departement;
+	}
+	public String getCommune() {
+		return commune;
+	}
+	public void setCommune(String commune) {
+		this.commune = commune;
+	}
+	public String getAffectataire() {
+		return affectataire;
+	}
+	public void setAffectataire(String affectataire) {
+		this.affectataire = affectataire;
+	}
+	public String getNiveauDeProtection() {
+		return niveauDeProtection;
+	}
+	public void setNiveauDeProtection(String niveauDeProtection) {
+		this.niveauDeProtection = niveauDeProtection;
+	}
+	public String getAdresse1() {
+		return adresse1;
+	}
+	public void setAdresse1(String adresse1) {
+		this.adresse1 = adresse1;
+	}
+	public String getCommune1() {
+		return Commune1;
+	}
+	public void setCommune1(String commune1) {
+		Commune1 = commune1;
+	}
+	public Integer getCodeDepartement() {
+		return codeDepartement;
+	}
+	public void setCodeDepartement(Integer codeDepartement) {
+		this.codeDepartement = codeDepartement;
+	}
+	public String getAncienneRegion() {
+		return ancienneRegion;
+	}
+	public void setAncienneRegion(String ancienneRegion) {
+		this.ancienneRegion = ancienneRegion;
+	}
+	public String getDMAJ() {
+		return DMAJ;
+	}
+	public void setDMAJ(String dMAJ) {
+		DMAJ = dMAJ;
+	}
+	public String getReference() {
+		return reference;
+	}
+	public void setReference(String reference) {
+		this.reference = reference;
+	}
+	public String getBase() {
+		return base;
+	}
+	public void setBase(String base) {
+		this.base = base;
+	}
+	public Integer getINSEE() {
+		return INSEE;
+	}
+	public void setINSEE(Integer iNSEE) {
+		INSEE = iNSEE;
+	}
+	public String getCoordonneesBan() {
+		return coordonneesBan;
+	}
+	public void setCoordonneesBan(String coordonneesBan) {
+		this.coordonneesBan = coordonneesBan;
+	}
+
 	
 
 }
